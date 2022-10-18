e9867146fc89232e40dce4f8cc44cecf7ca96538
joseph <jsphmariga@gmail.com>
2022-10-18 02:33:41 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that gather data from an API
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos/?userId: {}".format(sys.argv[1])).json()
+    completed = [i.get("title") for i in todos if i.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
