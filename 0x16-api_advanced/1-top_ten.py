3daa65704aa4e8639c165cc657f282627f463f1c
joseph <jsphmariga@gmail.com>
2022-11-02 02:11:02 +0000

    Top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""""Function"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            "User-Agent": "my-app.0.1"
+            }
+    payload = {
+            "limit": 10
+            }
+    res = requests.get(url, headers=headers, params=payload)
+    if res.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(h.get("data").get("title")) for h in results.get("children")]
