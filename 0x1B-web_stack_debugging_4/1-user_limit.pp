e6a20dc312324e6805d730e31ec99d5bac43ece2
joseph <jsphmariga@gmail.com>
2022-11-10 11:18:35 +0000

    Add configuration that will increase soft file limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,3 +4,7 @@ exec {"increase limit for hard file":
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+exec {"increase limit for soft file":
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
