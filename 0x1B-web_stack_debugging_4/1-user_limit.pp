f976ee4ae9f1285d5116918dc0bd33870e6d58be
joseph <jsphmariga@gmail.com>
2022-11-10 15:37:05 +0000

    Fix code

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,7 +4,8 @@ exec {"increase limit for hard file":
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+
 exec {"increase limit for soft file":
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/holberton soft/s/6/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
